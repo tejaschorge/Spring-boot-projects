fe031a4432772f4ae187c8905ea9dccec74769f9
Tejas Chorge <68380628+tejaschorge@users.noreply.github.com>
2025-08-11 21:26:42 +0530

    Connecting to H2 database spring boot code

---
+++ Connecting to H2 Database/src/main/java/com/Tejas/Database/DatabaseApplication.java
@@ -0,0 +1,30 @@
+package com.Tejas.Database;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@Log
+public class DatabaseApplication implements CommandLineRunner {
+	private final DataSource dataSource;
+
+	public DatabaseApplication(final DataSource dataSource){
+		this.dataSource = dataSource;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... args){
+		log.info("Datasource" + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("Select 1");
+	}
+}
